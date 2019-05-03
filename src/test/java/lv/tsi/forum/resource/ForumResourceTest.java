08085cdb5f97a623d51f8574f1effee30dedea9e
BOCbMOU <bocbou@gmail.com>
2019-05-03 11:49:45 +0300

    practical task

---
+++ src/test/java/lv/tsi/forum/resource/ForumResourceTest.java
@@ -0,0 +1,26 @@
+package lv.tsi.forum.resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ForumResourceTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getAllTopics() throws Exception {
+        mockMvc.perform(get("/forum/getAll")).andExpect(status().isOk());
+    }
+}
